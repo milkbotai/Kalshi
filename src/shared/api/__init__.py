90117e07e9fe78b9715705197d30724b9355df0b
Milkbot <you@yourdomain.com>
2026-01-25 21:35:20 -0500

    refactor: consolidate rate_limiter imports to single line

--- src/shared/api/__init__.py
+++ src/shared/api/__init__.py
@@ -2,11 +2,7 @@
 
 from src.shared.api.kalshi import KalshiClient
 from src.shared.api.nws import NWSClient
-from src.shared.api.rate_limiter import (
-    RateLimiterManager,
-    TokenBucket,
-    get_rate_limiter_manager,
-)
+from src.shared.api.rate_limiter import RateLimiterManager, TokenBucket, get_rate_limiter_manager
 
 __all__ = [
     "NWSClient",
